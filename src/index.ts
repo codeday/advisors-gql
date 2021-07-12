567d3760b39002d2148caff22293255c170731aa
Tyler Menezes <tylermenezes@codeday.org>
2021-07-12 14:22:19 -0400

    Fix availability finder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
-// import { startIntroducing } from './matching';
+import { startIntroducing } from './matching';
 import server from './server';
 import { registerDi } from './di';
 
 (async () => {
   registerDi();
-  // startIntroducing();
+  startIntroducing();
   server();
 })();
--- src/matching/availability.ts
+++ src/matching/availability.ts
@@ -6,17 +6,30 @@ import { RequestType } from '../enums';
 export type AdvisorWithRemaining = Advisor & { remaining: Record<RequestType, number> };
 
 function calculateRemainingAssignments(perWeek: number, assignmentsThisMonth: { createdAt: Date }[]): number {
-  const maxAccumulated = Math.max(perWeek > 0 ? 1 : 0, perWeek);
-  const startOfWeek = DateTime.local().startOf('week');
+  if (perWeek === 0) return 0;
+  const perFortnight = Math.ceil(perWeek * 2);
+  const perMonth = Math.floor(perWeek * 4);
+
+  const startOfWeek = DateTime.now().startOf('week');
+  const startOfFortnight = DateTime.now().startOf('week').minus({ weeks: 1 });
   const assignmentsThisWeek = assignmentsThisMonth.filter((a) => DateTime.fromJSDate(a.createdAt) > startOfWeek);
-  const remainingWithoutAccumulation = perWeek - assignmentsThisWeek.length;
-  if (remainingWithoutAccumulation > 0) return Math.ceil(remainingWithoutAccumulation);
-  return Math.ceil(maxAccumulated - assignmentsThisMonth.length);
+  const assignmentsThisFortnight = assignmentsThisMonth
+    .filter((a) => DateTime.fromJSDate(a.createdAt) > startOfFortnight);
+
+  // Calculate remaining
+
+  const remainingThisWeek = perWeek - assignmentsThisWeek.length;
+  const remainingThisFortnight = perFortnight - assignmentsThisFortnight.length;
+  const remainingThisMonth = perMonth - assignmentsThisMonth.length;
+
+  // Don't give someone extra assignments two weeks in a row, even if it would be under their monthly limit.
+  const roundFn = (remainingThisMonth > 0 && remainingThisFortnight > 0) ? Math.ceil : Math.floor;
+  return Math.max(0, roundFn(Math.min(remainingThisWeek, remainingThisFortnight, remainingThisMonth)));
 }
 
 export async function getAvailableAdvisors(): Promise<AdvisorWithRemaining[]> {
   const prisma = Container.get(PrismaClient);
-  const startOfMonth = DateTime.local().startOf('month').toJSDate();
+  const startOfMonth = DateTime.now().minus({ months: 1 }).toJSDate();
 
   // TODO(@tylermenezes): This should be possible to do with groupBy, but at the time of writing it is broken, and there
   // aren't enough advisors/requests for there to be a performance implication.
